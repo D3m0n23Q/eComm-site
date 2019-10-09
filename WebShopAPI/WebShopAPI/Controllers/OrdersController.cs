c90722778bf5707173a9277bc54b0f37cfa9ff96
Damian Evans <z3464512@student.unsw.edu.au>
2019-10-09 20:34:13 +1100

    Add GetShippingCost action to orders controller

---
+++ WebShopAPI/WebShopAPI/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebShopAPI.Entities;
+
+namespace WebShopAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+        private const decimal SHIPPING_COST_BOUNDARY = 50M;
+        private const decimal SHIPPING_COST_SMALL_ORDER = 10M;
+        private const decimal SHIPPING_COST_LARGE_ORDER = 20M;
+
+        // POST api/Orders
+        [HttpPost("Create")]
+        public StatusCodeResult CreateOrder([FromBody] IEnumerable<Product> products) => this.Ok();
+
+        [HttpGet("ShippingCost")]
+        public ActionResult<decimal> GetShippingCost([FromBody] IEnumerable<Product> products) => products.Sum(p => p.Value) < SHIPPING_COST_BOUNDARY ? SHIPPING_COST_SMALL_ORDER : SHIPPING_COST_LARGE_ORDER; 
+
+    }
+
+}

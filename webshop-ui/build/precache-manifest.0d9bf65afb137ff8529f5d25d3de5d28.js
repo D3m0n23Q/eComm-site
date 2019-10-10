0f15ed22c6094bfea2c6ba54439e7c1397a322d5
Damian Evans <damiantevans91@gmail.com>
2019-10-11 06:27:03 +1100

    no message

---
+++ webshop-ui/build/precache-manifest.0d9bf65afb137ff8529f5d25d3de5d28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f1e30cc2e86b7cb7973350354f7ec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc42a843e29199bcac76",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f8eaceaff8c6889691f4",
+    "url": "/static/js/2.54110292.chunk.js"
+  },
+  {
+    "revision": "bc42a843e29199bcac76",
+    "url": "/static/js/main.014da8a9.chunk.js"
+  },
+  {
+    "revision": "4bbb76b6ea736d10d004",
+    "url": "/static/js/runtime-main.74c385bc.js"
+  }
+]);

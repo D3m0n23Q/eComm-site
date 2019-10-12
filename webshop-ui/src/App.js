7e98a02398251614f1a2a7550874b3bd0158ccff
Damian Evans <damiantevans91@gmail.com>
2019-10-12 21:04:12 +1100

    Refactor App component

--- webshop-ui/src/App.js
+++ webshop-ui/src/App.js
@@ -1,53 +1,26 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {CheckoutScreen} from './Components/CheckoutScreen'
 import {ProductsScreen} from './Components/ProductsScreen'
 import { ThankYouScreen } from './Components/ThankYouScreen';
 import { NavBar } from './Components/NavBar';
+import { Routes } from './Routes';
 
 export default class App extends Component {
   constructor() {
     super();
     this.ShoppingCart = {CartValue: 0, Products: []}
-    this.checkout=this.checkout.bind(this);
-    this.navigateHome = this.navigateHome.bind(this);
   }
 
   render () {
     return (
     <div>
       <NavBar checkout={this.checkout} navigateHome={this.navigateHome}/>
-      <div id="div-content">
-        <ProductsScreen addProductToCart={this.addProductToCart.bind(this)} checkout={this.checkout.bind(this)}/>
-      </div>
+      <Routes cart={this.ShoppingCart}/>
+      <ToastContainer />
     </div>);
-  }
-
-  addProductToCart(product) {
-    var _Product = this.ShoppingCart.Products.find((elem) => elem.id == product.id)
-
-    if(!_Product) { 
-      _Product = product;
-      _Product.count = 1;
-      this.ShoppingCart.Products.push(_Product);
-    }
-    else {
-      _Product.count += 1;
-    }
-
-    this.ShoppingCart.CartValue += _Product.value;
-  }
-  
-  checkout() {
-    ReactDOM.render(<CheckoutScreen cart={this.ShoppingCart} createOrder={this.createOrder.bind(this)}/>, document.getElementById('div-content'));
-  }
-
-  createOrder() {
-    ReactDOM.render(<ThankYouScreen/>, document.getElementById('div-content'));
-  }
-  
-  navigateHome() {
-      ReactDOM.render(<ProductsScreen addProductToCart={this.addProductToCart.bind(this)}/>, document.getElementById('div-content'));
   }
 }
 

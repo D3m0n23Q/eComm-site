e737520ead370be3601ba17e7a4f4b78b6228f51
Damian Evans <damiantevans91@gmail.com>
2019-10-14 22:09:50 +1100

    Create Home.jsx

---
+++ webshop-ui/src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import { Container, Col, Row } from 'reactstrap'
+
+export class Home extends Component {
+    render() {
+        var component = (
+        <Col>
+            <h1>Welcome to eComm Inc.!</h1>
+            <p>This page is currently under construction, please use the nav bar above to navigate.</p>
+        </Col>
+        );
+
+
+        return component;
+    }
+} 

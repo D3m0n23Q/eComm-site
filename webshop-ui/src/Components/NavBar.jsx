7bb46e38c4f00444221e1d6ed4dbcbb64d8ddccc
Damian Evans <damiantevans91@gmail.com>
2019-10-14 22:15:02 +1100

    Update NavBar.jsx

--- webshop-ui/src/Components/NavBar.jsx
+++ webshop-ui/src/Components/NavBar.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem } from 'reactstrap';
 import { Link } from "react-router-dom";
 
 export class NavBar extends Component {
+    constructor() {
+        super();
+        this.state = {isOpen: false};
+        this.toggle = () => this.setState(prevState => ({isOpen: !prevState.isOpen}));
+    }
+
+
     render() {
         return (
-            <Navbar fixed="top">
+            <Navbar color="light" fixed="top" light>
                 <NavbarBrand>
-                    <Link to="/home" style={{color: '#ccc'}}>eComm Inc.</Link>
+                    <Link to="/home" style={{color: 'black'}}>eComm Inc.</Link>
                 </NavbarBrand>
-                <Link to="/checkout" style={{color: '#ccc'}}>Shopping Cart</Link>
+                <NavbarToggler onClick={this.toggle} className="ml-auto mr-3"/>
+                <Link  to="/checkout" style={{ color: 'black' }}>Shopping Cart</Link>
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav>
+                        <NavItem className="ml-auto mr-auto">
+                            <Link to="/products" onClick={this.toggle} style={{ color: 'black' }}>Products</Link>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
             </Navbar>
         );
     }

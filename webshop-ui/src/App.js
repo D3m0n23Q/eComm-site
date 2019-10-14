78d75349042739ec6fcc453b6c20415cded718d6
Damian Evans <damiantevans91@gmail.com>
2019-10-14 22:22:59 +1100

    Add router and cookie check
    Wraps components in  a router to ensure testing passes.
    
    Also changed the way the cookie was being deserialised to ensure a null cookie wasn't being deserialised.

--- webshop-ui/src/App.js
+++ webshop-ui/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ToastContainer } from "react-toastify";
+import { BrowserRouter as Router } from 'react-router-dom';
 import "react-toastify/dist/ReactToastify.css";
 import { NavBar } from './Components/NavBar';
 import { Routes } from './Routes';
@@ -8,17 +9,20 @@ import { Footer } from './Components/Footer'
 export default class App extends Component {
   constructor() {
     super();
-    this.Cookie = JSON.parse(this.getCookie('cart'));
-    this.ShoppingCart = this.Cookie ? this.Cookie : {CartValue: 0, Products: []}
+    var cookie = this.getCookie('cart');
+    var cart = cookie ? JSON.parse(cookie) : null;
+    this.ShoppingCart = cart ? cart : {CartValue: 0, Products: []}
   }
 
   render () {
     return (
     <div>
-      <NavBar checkout={this.checkout} navigateHome={this.navigateHome}/>
-      <Footer/>
-      <Routes cart={this.ShoppingCart} getCookie={this.getCookie()}/>
-      <ToastContainer />
+      <Router>
+        <NavBar checkout={this.checkout} navigateHome={this.navigateHome}/>
+        <Footer/>
+        <Routes cart={this.ShoppingCart} getCookie={this.getCookie()}/>
+        <ToastContainer />
+      </Router>
     </div>);
   }
 
